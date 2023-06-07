f33f0934bac557563a25ba886d47228d2706107f
night780 <night780j@gmail.com>
2023-06-07 14:28:32 -0700

    Colors

--- app/src/main/java/com/example/finapp/MainActivity.java
+++ app/src/main/java/com/example/finapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.finapp;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
         financialInsightsButton = findViewById(R.id.financialInsightsButton);
         expenseHistoryButton = findViewById(R.id.expenseHistoryButton);
         totalBudgetTextView = findViewById(R.id.totalBudgetTextView);
+
+
+        expenseTrackerButton.setTextSize(22);
+        budgetManagementButton.setTextSize(22);
+        financialInsightsButton.setTextSize(22);
+        expenseHistoryButton.setTextSize(22);
+
+
+
 
         expenseTrackerButton.setOnClickListener(new View.OnClickListener() {
             @Override
